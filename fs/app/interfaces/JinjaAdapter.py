fba859fee5435a3891ffd24b7ab94edff5003f14
BearingMe <gomes.bruno201@gmail.com>
2024-02-25 23:57:51 -0300

    feat: JinjaAdapter

---
+++ fs/app/interfaces/JinjaAdapter.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict, List
+from abc import ABC, abstractmethod
+
+class JinjaAdapter(ABC):
+    @abstractmethod
+    def set_template(self, template_path: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_data(self, data: Dict[any]) -> None:
+        pass
+
+    @abstractmethod
+    def set_json(self, json_path: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_env(self, env_path: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_stylesheet(self, stylesheet_path: str) -> None:
+        pass
+
+    @abstractmethod
+    def render(self) -> str:
+        pass
+
