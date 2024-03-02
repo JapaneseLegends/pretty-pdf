3712fe3a6d075f75a456bbd8d5892891e15ecb46
BearingMe <gomes.bruno201@gmail.com>
2024-03-01 22:40:40 -0300

    feat: impl TemplateEngineAdapter class

---
+++ fs/adapters/TemplateEngineAdapter.py
@@ -0,0 +1,24 @@
+from typing import Any, TypeVar
+from ..app.interfaces.TemplateEngine import TemplateEngine
+from jinja2 import Environment, FileSystemLoader
+
+
+class TemplateEngineAdapter(TemplateEngine):
+    def __init__(self, template: str):
+        self.template = template
+        self.data = {}
+
+    def set_template(self, template_path: str) -> TemplateEngine:
+        self.template = template_path
+        return self
+
+    def set_data(self, data: dict[str, Any]) -> TemplateEngine:
+        self.data = data
+        return self
+
+    def render(self) -> str:
+        env = Environment(loader=FileSystemLoader('templates'))
+        template = env.get_template(self.template)
+        
+        return template.render(self.data)
+
